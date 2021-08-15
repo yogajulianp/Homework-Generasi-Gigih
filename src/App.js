f93369dc1a40fee58e1060ee05ef4b5ab73c2cf5
Yoga Julian Prasutiyo <yogajprasutiyo@gmail.com>
2021-08-15 08:11:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Yoga Julian Prasutiyo</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          classNtame="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
